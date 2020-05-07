d95db659f078e897eae75a929dd798c854c827f0
nayakrahul <rahulnk521@gmail.com>
2020-05-07 11:49:30 +0000

    Deploying to gh-pages from  @ e963433f12c6f39af573db3a382400c746a7046d 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.50f2d18ec888237e3fed81bac397b19e.js"
+  "/precache-manifest.a0325ee19daff44b6de6a7f7257295b7.js"
 );
 
 self.addEventListener('message', (event) => {
